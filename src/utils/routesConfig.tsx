bba6fd380b645174cfbe73eb3a9d113f9eba2467
Tony Robertson <admin@tonyrobertson.dev>
2023-05-25 20:22:32 -0700

    Add profile pic and temp msg

--- src/utils/routesConfig.tsx
+++ src/utils/routesConfig.tsx
@@ -9,7 +9,7 @@ const routesConfig = [
       <>
         {/* Header, sidebar and other fixed components go here */}
         <div className="flex justify-center items-center w-screen h-screen backdrop-blur-lg">
-          <main className=" w-[50rem] h-5/6 bg-slate-800 rounded-xl drop-shadow-lg bg-gradient-to-tr from-gray-900 to-gray-600 bg-gradient-to-r">
+          <main className=" w-[50rem] h-5/6 bg-slate-800 rounded-xl drop-shadow-lg bg-gradient-to-tr from-slate-950 to-slate-800 bg-gradient-to-r">
             <Outlet />
           </main>
         </div>
