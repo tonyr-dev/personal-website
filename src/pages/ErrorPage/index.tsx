d7f1cf9d300621ebddb5d0f490f95d76d52f830e
Tony Robertson <admin@tonyrobertson.dev>
2023-05-25 20:35:58 -0700

    Hotfix: Fix case of error being null

--- src/pages/ErrorPage/index.tsx
+++ src/pages/ErrorPage/index.tsx
@@ -1,15 +1,25 @@
+import React from 'react';
 import { useRouteError } from 'react-router-dom';
 import './index.css';
 
+type ErrorType = {
+  statusText?: string;
+  message?: string;
+};
+
 function ErrorPage() {
-  const error = useRouteError();
+  const error = useRouteError() as ErrorType;
 
   return (
     <div id="error-page">
       <h1>Oops!</h1>
       <p>Sorry, an unexpected error has occurred.</p>
       <p>
-        <i>{error.statusText || error.message}</i>
+        <i>
+          {error?.statusText ||
+            error?.message ||
+            'An unknown error has occurred'}
+        </i>
       </p>
     </div>
   );
